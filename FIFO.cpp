db5f09e0e0670de382d92c6aa2788e345d5a157f
Shoaib Ahmed <shoaib30@users.noreply.github.com>
2015-09-12 10:53:09 +0530

    Create FIFO.cpp

---
+++ FIFO.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+	int initial,n,i,j,queues[50];
+	int total=0;
+	cout<<"Enter the initial position of disk head (cylinder at which head is): ";
+	cin>>initial;
+	cout<<"Enter the number of requests for disk access: ";
+	cin>>n;
+	cout<<"Enter the cylinder number to access \n";
+	for(i=0; i<n; i++)
+    {
+        cout<<i+1<<":";
+		cin>>queues[i];
+    }
+	total=abs(initial-queues[0]);
+	for(i=0; i<n-1; i++)
+	{
+			total+=abs(queues[i+1]-queues[i]);
+	}
+	cout<<"Total head movements: "<<total;
+    return 0;
+}
+
